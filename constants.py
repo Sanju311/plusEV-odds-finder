1d308071720cdfa38e433207716cd4f1fcce59a9
sanju311 <sanjumoorthy622@gmail.com>
2024-06-27 19:16:41 -0400

    added new functions to constants page, set up main method and general control flow, set up ISO time configurations

--- constants.py
+++ constants.py
@@ -1,25 +1,34 @@
+from datetime import datetime, timezone, timedelta
+
+
 API_KEY = "63971622c64f6ae72dc229f7eb1fbf76"
-THE_ODDS_API_BASE = "https://api.the-odds-api.com"
+THE_ODDS_API_BASE = "https://api.the-odds-api.com/v4/sports/"
 EV_THRESHOLD = ""
 
-
 sports = {
-    "hockey" : "icehockey_nhl",
-    "euro" : "soccer_uefa_european_championship" 
+    "soccer_uefa_european_championship", 
+    "americanfootball_nfl",
+    "basketball_nba",
+    "soccer_usa_mls"
 }
 
-regions = {
-    "us1" : "us",
-    "us2" : "us2",
-    "uk" : "uk"
-}
+us_books = "betmgm,betrivers,betus,draftkings,fanduel,pointsbet"
+uk_books = "betway"
+eu_books = "pinnacle"
+all_books = us_books+uk_books+eu_books
+
+regions = "us"
 
 markets = {
     "moneyline": "h2h",
     "spread": "spreads",
-
-
+    "total": "totals",
+    "team_toal": "team_totals",
+    "bothToScore": "btts",
+    "alt_spread": "alternate_spreads"
 }
+
+main_markets = "h2h,spreads,totals"
 
 endpoints = {
     "getSports": THE_ODDS_API_BASE+"/v4/sports/?apiKey="+API_KEY,
@@ -27,6 +36,16 @@ endpoints = {
     "getEventOdds" : THE_ODDS_API_BASE+ "/v4/sports/"+sports["euro"]+"/events/++/odds/?apiKey="+API_KEY+"&regions="+regions["us1"]+ "&markets="+markets["spread"]
 }
 
-def getEvents(eventID):
-    #return THE_ODDS_API_BASE+"/v4/sports/"+sports["euro"]+"/events/"+eventID+"/odds?apiKey="+API_KEY+"&regions="+regions["us2"]
-    return THE_ODDS_API_BASE+"/v4/sports/"+sports["euro"]+"/events/"+eventID+"/odds?apiKey="+API_KEY+"&bookmakers=pinnacle"
+#returns the events API
+def getEventsAPI(sport):
+    return THE_ODDS_API_BASE+sport+"/events?apiKey="+API_KEY
+
+def getAnyEventOddsAPI(sport, eventID):
+    return THE_ODDS_API_BASE+sport+"/odds/?apiKey="+API_KEY+"&regions="
+
+def getEventOddsAPI(sport, eventID):
+    time_now = datetime.now(timezone.utc)
+    time_plus_week = (time_now + timedelta(days=7)).isoformat()
+    time_plus_week = time_plus_week[:-6] + "Z"
+
+    return THE_ODDS_API_BASE+sport+"/odds/?apiKey="+API_KEY+"&eventIds="+eventID+"&regions="+regions+"&markets="+main_markets+"&commenceTimeTo="+time_plus_week+"&bookmakers="+all_books
--- main.py
+++ main.py
@@ -1,15 +1,76 @@
 from constants import endpoints
 from constants import sports
 from constants import regions
-from constants import getEvents
+from constants import getEventsAPI
 
 import requests
 import json
+import redis
 
 
 
+def main(): 
+    while True:
+        findBets()
+
+
+def findBets():
+    
+    for sport in sports:
+        
+        try:
+
+            events = requests.get(getEventsAPI(sport))
+            eventsJSON = events.json()
+
+            for event in eventsJSON:
+
+                currEvent = []
+                currEvent['id'] = event['id']
+                currEvent['title'] = event['sport_title']
+                currEvent['teams'] = event['home_team'] + event['away_team']           
+
+                findOdds(currEvent)    
+
+        except requests.exceptions.ConnectionError as e:
+            print("Connection error:" + e)
+
+        except requests.exceptions.RequestException as e:
+            print("Request library error:" + e)   
+
+        except Exception as e:
+            print("General error:" + e)
+
+
+        time.sleep(10)
+
+
+
+def findOdds(event):
+    print(event)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+# r = redis.Redis(host="localhost", port=6379)
+# r.set("france","germafsfsfnu")
+
+# print(r.get("france"))
+
+
+# for event in data:
+#     eventID = event['id']
+#     eventTime = event['commence_time']
+#     eventName = event['home_team'] + event['away_team']
+
+    
 # response = requests.get(endpoints['getEvents'])
-
 
 # if response.status_code == 200:
 #     data = response.json()
@@ -20,20 +81,11 @@ import json
 #     print("error getting api data")
 
 
+# eventAPI = getEvents('d30e31303173a5724c8a8c118c4887e4')
+# print(eventAPI)
+# response2 = requests.get(eventAPI)
+# data = response2.json()
 
-eventAPI = getEvents('d30e31303173a5724c8a8c118c4887e4')
-print(eventAPI)
-response2 = requests.get(eventAPI)
-data = response2.json()
-
-file_path = "nether_austria_pinnacle.json"
-with open(file_path,'w') as json_file:
-    json.dump(data,json_file, indent=4)
-
-# for event in data:
-#     eventID = event['id']
-#     eventTime = event['commence_time']
-#     eventName = event['home_team'] + event['away_team']
-
-    
-
+# file_path = "nether_austria_pinnacle.json"
+# with open(file_path,'w') as json_file:
+#     json.dump(data,json_file, indent=4)
